4f5957c77812fa6ff4a08864b57f74f64b3330c7
threadripper <ethanatkinson11@icloud.com>
2026-03-10 16:06:06 -0600

    Fixed file structure

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ import express from 'express';
 import { fileURLToPath } from 'url';
 import path from 'path';
 import { testConnection } from './src/models/db.js';
+import { getAllOrganizations } from './src/models/organizations.js';
 
 // Define the the application environment
 const NODE_ENV = process.env.NODE_ENV?.toLowerCase() || 'production';
@@ -36,6 +37,9 @@ app.get('/', async (req, res) => {
 });
 
 app.get('/organizations', async (req, res) => {
+    const organizations = await getAllOrganizations();
+    console.log(organizations);
+      
     const title = 'Our Partner Organizations';
     res.render('organizations', { title });
 });
