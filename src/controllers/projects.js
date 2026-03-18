bf5428e8fe3bde4f6b01ad4241eaf4b0823a1e7b
threadripper <ethanatkinson11@icloud.com>
2026-03-18 14:10:43 -0600

    Completed project views

--- src/controllers/projects.js
+++ src/controllers/projects.js
@@ -1,13 +1,26 @@
 // Import any needed model functions
 import { getAllProjects } from '../models/projects.js';
+import { getUpcomingProjects } from '../models/projects.js';
+import { getProjectDetails } from '../models/projects.js';
+
+const NUMBER_OF_UPCOMING_PROJECTS = 5;
 
 // Define any controller functions
 const showProjectsPage = async (req, res) => {
-    const projects = await getAllProjects();
+    const projects = await getUpcomingProjects(NUMBER_OF_UPCOMING_PROJECTS);
     const title = 'Service Projects';
 
     res.render('projects', { title, projects });
 };  
 
+const showProjectDetailsPage = async (req, res) => {
+    const projectId = req.params.id;
+    const projectDetails = await getProjectDetails(projectId);
+    const title = projectDetails ? projectDetails.title : 'Project Details';
+
+    res.render('project-details', { title, projectDetails });
+};
+
+
 // Export any controller functions
-export { showProjectsPage };
+export { showProjectsPage, showProjectDetailsPage };
--- src/models/projects.js
+++ src/models/projects.js
@@ -33,4 +33,33 @@ const getProjectsByOrganizationId = async (organizationId) => {
       return result.rows;
 };
 
-export { getAllProjects, getProjectsByOrganizationId };
+const getUpcomingProjects = async (number_of_projects) => {
+  const query = `
+    SELECT sp.project_id, sp.organization_id, sp.title, sp.description, sp.date, sp.location,
+         o.name AS organization_name
+      FROM public.service_project sp
+      JOIN public.organization o ON sp.organization_id = o.organization_id
+     WHERE sp.date >= CURRENT_DATE
+     ORDER BY sp.date ASC
+     LIMIT $1;
+  `;
+
+  const params = [number_of_projects];
+  const result = await db.query(query, params);
+  return result.rows;
+};
+
+const getProjectDetails = async (id) => {
+    const query = `
+        SELECT sp.project_id, sp.organization_id, sp.title, sp.description, sp.date, sp.location,
+               o.name AS organization_name
+          FROM public.service_project sp
+          JOIN public.organization o ON sp.organization_id = o.organization_id
+         WHERE sp.project_id = $1;
+    `;
+ 
+    const result = await db.query(query, [id]);
+    return result.rows[0];
+};
+
+export { getAllProjects, getProjectsByOrganizationId, getUpcomingProjects, getProjectDetails };
